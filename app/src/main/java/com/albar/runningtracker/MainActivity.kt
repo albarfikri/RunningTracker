dbc995c2e389129eb613af79d65c6b295a605cff
albarfikri <fikrialbar4@gmail.com>
2022-03-01 12:01:02 +0700

    first Commit

---
+++ app/src/main/java/com/albar/runningtracker/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.albar.runningtracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.albar.runningtracker.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
