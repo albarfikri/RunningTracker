0b397dc98e76ba70e2a29108b726354dd746c91d
albarfikri <fikrialbar4@gmail.com>
2022-03-01 21:44:31 +0700

    Set Up User Dao

---
+++ app/src/main/java/com/albar/runningtracker/db/RunDAO.kt
@@ -0,0 +1,41 @@
+package com.albar.runningtracker.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface RunDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    // suspend will be executed in coroutine
+    suspend fun insertRun(run: Run)
+
+    @Delete
+    suspend fun deleteRun(run: Run)
+
+    @Query("SELECT * FROM running_table ORDER BY timeStamp DESC")
+    fun getAllRunSortedByDate(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY timeInMillis DESC")
+    fun getAllRunSortedByTimeInMillis(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY caloriesBurned DESC")
+    fun getAllRunSortedByCaloriesBurned(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY avgSpeedInKMH DESC")
+    fun getAllRunSortedByAvgSpeed(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY distanceInMeters DESC")
+    fun getAllRunSortedByDistance(): LiveData<List<Run>>
+
+    @Query("SELECT SUM(timeInMillis) FROM running_table")
+    fun getTotalInMillis():LiveData<Long>
+
+    @Query("SELECT SUM(caloriesBurned) FROM running_table")
+    fun getTotalCaloriesBurned():LiveData<Int>
+
+    @Query("SELECT SUM(distanceInMeters) FROM running_table")
+    fun getTotalDistance():LiveData<Int>
+
+    @Query("SELECT AVG(avgSpeedInKMH) FROM running_table")
+    fun getTotalAvgSpeed():LiveData<Float>
+}
