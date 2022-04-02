838565d85a5059369442ac4b7b34796e2d616c3c
albarfikri <fikrialbar4@gmail.com>
2022-04-02 23:13:18 +0700

    saving running record to database

--- app/src/main/java/com/albar/runningtracker/ui/fragment/TrackingFragment.kt
+++ app/src/main/java/com/albar/runningtracker/ui/fragment/TrackingFragment.kt
@@ -9,6 +9,7 @@ import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.albar.runningtracker.R
 import com.albar.runningtracker.databinding.FragmentTrackingBinding
+import com.albar.runningtracker.db.Run
 import com.albar.runningtracker.other.Constants.ACTION_PAUSE_SERVICE
 import com.albar.runningtracker.other.Constants.ACTION_START_OR_RESUME_SERVICE
 import com.albar.runningtracker.other.Constants.ACTION_STOP_SERVICE
@@ -21,9 +22,13 @@ import com.albar.runningtracker.service.TrackingService
 import com.albar.runningtracker.ui.viewmodels.MainViewModel
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.PolylineOptions
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
+import kotlin.math.round
 
 @AndroidEntryPoint
 class TrackingFragment : Fragment() {
@@ -40,6 +45,8 @@ class TrackingFragment : Fragment() {
     private var curTimeInMillis = 0L
 
     private var menu: Menu? = null
+
+    private var weight = 80f
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -109,6 +116,11 @@ class TrackingFragment : Fragment() {
             toggleRun()
         }
 
+        binding.btnFinishRun.setOnClickListener {
+            zoomToLookWholeTrack()
+            endRunAndStoreToDb()
+        }
+
         subscribeToObservers()
     }
 
@@ -162,6 +174,47 @@ class TrackingFragment : Fragment() {
                     MAP_ZOOM
                 )
             )
+        }
+    }
+
+    private fun zoomToLookWholeTrack() {
+        val bounds = LatLngBounds.Builder()
+        for (polyline in pathPoints) {
+            for (pos in polyline) {
+                bounds.include(pos)
+            }
+        }
+
+        map?.moveCamera(
+            CameraUpdateFactory.newLatLngBounds(
+                bounds.build(),
+                binding.mapView.width,
+                binding.mapView.height,
+                (binding.mapView.height * 0.05f).toInt()
+            )
+        )
+    }
+
+    private fun endRunAndStoreToDb() {
+        map?.snapshot { bmp ->
+            var distanceInMeters = 0
+            for (polyline in pathPoints) {
+                distanceInMeters += TrackingUtility.calculatePolylineLength(polyline).toInt()
+            }
+
+            val avgSpeed =
+                round((distanceInMeters / 1000f) / (curTimeInMillis / 1000f / 60 / 60) * 10) / 10f
+            val dateTimeStamp = Calendar.getInstance().timeInMillis
+            val caloriesBurned = ((distanceInMeters / 1000f) * weight).toInt()
+            val run =
+                Run(bmp, dateTimeStamp, avgSpeed, distanceInMeters, curTimeInMillis, caloriesBurned)
+            viewModel.insertRun(run)
+            Snackbar.make(
+                requireActivity().findViewById(R.id.rootView),
+                "Run saved successfully",
+                Snackbar.LENGTH_LONG
+            ).show()
+            stopRun()
         }
     }
 
--- app/src/main/java/com/albar/runningtracker/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/albar/runningtracker/ui/viewmodels/MainViewModel.kt
@@ -1,12 +1,19 @@
 package com.albar.runningtracker.ui.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.albar.runningtracker.db.Run
 import com.albar.runningtracker.repositories.MainRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
     val mainRepository: MainRepository
 ) : ViewModel() {
+
+    fun insertRun(run: Run) = viewModelScope.launch {
+        mainRepository.insertRun(run)
+    }
 }
